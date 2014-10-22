e9eaccc5f11529881dd025be21daefe626e95abc
Laura Barreto <labarreto@vassar.edu>
2014-10-22 00:20:29 -0400

    testing MT cardinality

--- src/data2/TestTest.java
+++ src/data2/TestTest.java
@@ -11,11 +11,27 @@ package data2;
  * @author ldbruby95
  */
 public class TestTest {
+    static int count;
+    static finiteBag mt = new SBBT_MT();
     
     
-    
+        public static void testCardinalityMT(finiteBag u) {
+        // first check that it's empty. 
+        if (u.isEmptyHuh()) {
+        
+            if (u.cardinality() != 0) {
+                
+                System.out.println("Failure! Cardinality for empty set is: " + u.cardinality()
+                        + ", should be: 0");
+            }
+        } 
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("------------------------------------------------------------");
+        System.out.println("TESTING EMPTY . . . ");
+        testCardinalityMT(mt);
     }
     
     
